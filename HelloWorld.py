cb3ea0dd266d381a5fd331a6f1036f8cf545a12a
dylanbaghel <baghelabhi39@gmail.com>
2018-01-23 10:37:07 -0800

    a name list created and added print function to provide random name display

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,9 @@
 import random
 
+name_people = ["Abhishek","Baghel","Akash","Dylan","Vikas"]
+
 def randomNumber():
-    return random.randint(1,6)
+    return random.randint(1,len(name_people) - 1)
 
 for i in range(10):
-    print(randomNumber())
+    print(name_people[randomNumber()])
