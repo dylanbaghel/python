5b3318b8035f103203534f2f97b6f59f7fbbefd4
dylanbaghel <baghelabhi39@gmail.com>
2018-01-23 10:48:41 -0800

    added color list also to provide random color with random names and numbers

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,10 +2,11 @@ import random
 
 name_people = ["Abhishek","Baghel","Akash","Dylan","Vikas"]
 name_number = [11,22,33,44,55,66,77,88]
+name_color = ['Red','Green','Blue','Yello','Magenta','violet']
 
 def randomNumber(number):
     return random.randint(1,number)
 
 for i in range(10):
-    print(name_people[randomNumber(len(name_people) - 1)],": ",name_number[randomNumber(len(name_people) - 1)])
+    print(name_people[randomNumber(len(name_people) - 1)],": ",name_number[randomNumber(len(name_number) - 1)], "------>>>> ",name_color[randomNumber(len(name_color) - 1)])
 print("\nDone")
