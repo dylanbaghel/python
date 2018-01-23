f4e6676d79932b6d7a09acf710e8dabdef14ab3c
dylanbaghel <baghelabhi39@gmail.com>
2018-01-23 10:41:12 -0800

    added number list also to provide random number with random names

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,9 +1,10 @@
 import random
 
 name_people = ["Abhishek","Baghel","Akash","Dylan","Vikas"]
+name_number = [11,22,33,44,55,66,77,88]
 
-def randomNumber():
-    return random.randint(1,len(name_people) - 1)
+def randomNumber(number):
+    return random.randint(1,number)
 
 for i in range(10):
-    print(name_people[randomNumber()])
+    print(name_people[randomNumber(len(name_people) - 1)],": ",name_number[randomNumber(len(name_people) - 1)])
